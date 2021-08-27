e0c8c261f26561a16c7cf463c41d356517b76be5
rafi2011 <rnessa2011@gmail.com>
2021-08-27 09:38:29 -0400

    Updates

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 //import { createUser } from '../utils/API';
-import { useMutation } from '@apollo/client'
+import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
